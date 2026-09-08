ccbfe3e99dfb946cc06cb61737efd5855e6eebb1
maxitabakman <49928297@est.ort.edu.ar>
2026-09-08 12:11:14 -0300

    oaid

--- functions.js
+++ functions.js
@@ -6,7 +6,9 @@ function login() {
     
 }
 export {login}
-function escucho() {
-    
-}
+const escucho = async (req, res) => {
+    const { token } = req.body;
+    await query("INSERT * INTO escucha INNER JOIN cancion ON tabla1.columna_comun = tabla2.columna_comun;", [nombre]);
+    res.status(201).json({ nombre });
+};
 export {escucho}
