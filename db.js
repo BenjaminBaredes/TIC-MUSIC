cdc14614fe251947e293c154de657fe875452c5e
Nicolaswid <nicolaswidder@gmail.com>
2026-09-08 11:49:20 -0300

    Conectadardopolis

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  host: "ep-sweet-cake-at7nxlx5-pooler.c-9.us-east-1.aws.neon.tech",
+  host: "ep-dark-night-a5fxpyxq-pooler.us-east-2.aws.neon.tech",
   user: "neondb_owner",
-  password: "npg_PuS0LEQzJ8fF",
+  password: "npg_gElsey5qwZn0",
   database: "neondb",
   port: 5432,
   ssl: {
