5455239bd0ffe78f40d5ba3889435774cd463a75
maxitabakman <49928297@est.ort.edu.ar>
2026-09-15 12:09:52 -0300

    lkh gny

--- functions.js
+++ functions.js
@@ -9,7 +9,6 @@ export {login}
 const escucho = async (req, res) => {
     const { token } = req.body;
     let tokendesencriptado = await jwt.verify(token,clave)
-    await query("INSERT * INTO escucha as esc INNER JOIN cancion as can ON esc.cancion_ID = can.ID INNER JOIN usuario as us ON esc.usuario_id = us.ID where us.id", [tokendesencriptado]);
-    res.status(201).json({ tokendesencriptado  });
+    res.json( await query("INSERT * INTO escucha as esc INNER JOIN cancion as can ON esc.cancion_ID = can.ID INNER JOIN usuario as us ON esc.usuario_id = us.ID where us.id", [tokendesencriptado]));
 };
 export {escucho}
