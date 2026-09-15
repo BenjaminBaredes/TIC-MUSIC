1ef960ef7c5454196269d7ce770d9db34e0c7103
maxitabakman <49928297@est.ort.edu.ar>
2026-09-15 11:50:19 -0300

    me faltan cosas

--- functions.js
+++ functions.js
@@ -8,7 +8,8 @@ function login() {
 export {login}
 const escucho = async (req, res) => {
     const { token } = req.body;
-    await query("INSERT * INTO escucha as esc INNER JOIN cancion as can ON esc.cancion_ID = can.ID INNER JOIN usuario as us ON esc.usuario_id = us.ID where us.id", [token]);
-    res.status(201).json({ token  });
+    let tokendesencriptado = await jwt.verify(token,clave)
+    await query("INSERT * INTO escucha as esc INNER JOIN cancion as can ON esc.cancion_ID = can.ID INNER JOIN usuario as us ON esc.usuario_id = us.ID where us.id", [tokendesencriptado]);
+    res.status(201).json({ tokendesencriptado  });
 };
 export {escucho}
